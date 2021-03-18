311468e636ca8de4304d8e65c892457e58262779
Partha <partha.narra123@gmail.com>
2021-03-18 22:35:15 +0530

    xmlHttpRequest Updated

--- JS/xmlHttpRequest.js
+++ JS/xmlHttpRequest.js
@@ -2,5 +2,8 @@ const url='https://api.chucknorris.io/jokes/random';
 
 const request=new XMLHttpRequest();
 request.open('GET',url);
+request.onload=()=>{
+    console.log(request.response)
+}
 request.responseType="json";
 request.send();
